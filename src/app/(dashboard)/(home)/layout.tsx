2cda33192c8d3f9678a635a59805cf2e78e298da
Kuba Ratajczyk <kuba.ratajczyk.biz@gmail.com>
2024-11-06 12:22:53 +0100

    Added authentication layer

--- src/app/(dashboard)/(home)/layout.tsx
+++ src/app/(dashboard)/(home)/layout.tsx
@@ -2,6 +2,7 @@ import BreadcrumbHeader from '@/components/BreadcrumbHeader';
 import { DesktopSidebar } from '@/components/Sidebar';
 import { ModeToggle } from '@/components/ThemeModeToggle';
 import { Separator } from '@/components/ui/separator';
+import { SignedIn, UserButton } from '@clerk/nextjs';
 import React from 'react';
 
 type DashboardLayoutProps = {
@@ -15,8 +16,11 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
 			<div className="flex flex-col flex-1 min-h-screen">
 				<header className="flex items-center justify-between px-6 py-4 h-[50px] container">
 					<BreadcrumbHeader />
-					<div className="gap-1 flex items-center">
+					<div className="gap-2 flex items-center">
 						<ModeToggle />
+						<SignedIn>
+							<UserButton />
+						</SignedIn>
 					</div>
 				</header>
 				<Separator />
